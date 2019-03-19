538e77aad1e88ff2c886e59132268b3ebecbb6bc
Shawn Ren <doggy-ren@hotmail.com>
2019-03-19 03:23:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  " + holiday + " " + supplies
     end
   end
-end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -72,7 +71,6 @@ end
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
